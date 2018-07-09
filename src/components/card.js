f9fca07459f17b1e7cc0097045346e26e3564e46
Eric <Broadbent52@aol.com>
2018-07-09 08:12:53 -0600

    clear working

--- src/components/card.js
+++ src/components/card.js
@@ -1,31 +1,32 @@
 import React, {Component} from 'react';
 import Input from './input';
 import Content from './content';
+const INITIAL_STATE = {
+        color: '',
+        pnoun: '',
+        adjectiveOne: '',
+        celebOne: '',
+        adjectiveTwo: '',
+        nounOne: '',
+        numberOne: '',
+        numberTwo: '',
+        nounTwo: '',
+        adjectiveThree: '',
+        celebTwo: '',
+        celebThree: '',
+        adjectiveFour: '',
+        nounThree: '',
+        celebFour: '',
+        adjectiveFive: '',
+        contentVisible: false     
+}
 
 class Card extends Component{
     constructor() {
         super()
 
-        this.state = {
-            color: '',
-            pnoun: '',
-            adjectiveOne: '',
-            celebOne: '',
-            adjectiveTwo: '',
-            nounOne: '',
-            numberOne: '',
-            numberTwo: '',
-            nounTwo: '',
-            adjectiveThree: '',
-            celebTwo: '',
-            celebThree: '',
-            adjectiveFour: '',
-            nounThree: '',
-            celebFour: '',
-            adjectiveFive: '',
-            contentVisible: false
+        this.state = INITIAL_STATE;
             
-        }
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleFormSubmit = this.handleFormSubmit.bind(this);
     }
@@ -41,12 +42,13 @@ class Card extends Component{
     handleFormSubmit(event){
         
         event.preventDefault()
-        this.setState({contentVisible: !this.state.contentVisible})
+
+        if (this.state.contentVisible){
+        this.setState(INITIAL_STATE)
+    }else{
+        this.setState({contentVisible: true})
     }
-
-
-
-
+    }
     render(){
        
         const inputData = [
@@ -81,7 +83,7 @@ class Card extends Component{
                 </div>
                 <button type="submit">{!this.state.contentVisible ? "Generate MadLibs" : "Clear Form" }</button>
                 {
-                    this.state.contentVisible ? <Content data={this.state}/>:<div>''</div>
+                    this.state.contentVisible ? <Content data={this.state}/>:''
                 }
                 {
                     // inputData.map((data, index) => {
